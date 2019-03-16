e983c58f53e3324b8c18dcc4c01bdaf2b6f76596
cdanielgomes <cdanielgomes@github.com>
2019-03-16 19:35:54 +0000

    cart

--- html/script.js
+++ html/script.js
@@ -29,6 +29,39 @@ paymentButton.addEventListener("click", function (event) {
 })
 
 
+let backAddress = document.getElementsByClassName("back-button")
+backAddress[0].addEventListener("click", backToAddress);
+backAddress[1].addEventListener("click", backToPayment);
+function backToAddress(event){
+event.preventDefault();
+let next = document.querySelector("#cart li:nth-child(1) a")
+let current = document.querySelector("#cart li:nth-child(2) a")
+let nextContent = document.querySelector("#address")
+let currentContent = document.querySelector("#payment")
+
+current.className = "nav-link"
+next.classList.add("active");
+currentContent.className = "tab-pane fade"
+nextContent.className = "tab-pane fade show active"
+
+}
+
+function backToPayment(event) {
+    event.preventDefault();
+    let next = document.querySelector("#cart li:nth-child(2) a")
+    let current = document.querySelector("#cart li:nth-child(3) a")
+    let nextContent = document.querySelector("#payment")
+    let currentContent = document.querySelector("#review")
+
+    current.className = "nav-link"
+    next.classList.add("active");
+    currentContent.className = "tab-pane fade"
+    nextContent.className = "tab-pane fade show active"
+
+}
+
+
+// CALCULATE PRICES
 let prices = document.getElementsByClassName("price");
 let qts = document.getElementsByClassName("quantity");
 let unitPrices = document.getElementsByClassName("subtotal");
