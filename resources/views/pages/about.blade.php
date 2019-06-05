206c5ef16af5612a4e9affb3612eeb3d421eafcd
Gonçalo Bernardo <up201606058@fe.up.pt>
2019-06-05 03:51:15 +0100

    Online help

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -178,5 +178,36 @@
   </div>
 </main>
 
+  <!-- Modal OnlineHelp -->
+  <p class="text-secondary">
+  <button type="button" class="btn" data-toggle="modal" data-target="#exampleModal">
+    <div class="container topright text-aling-top">
+      <i class="far topright fa-question-circle fa-2x text-top "></i>
+    </div>
+  </button>
+</p>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">What's the purpose of this page?</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <p><strong>Any questions</strong> outside of the range bolstered by “Frequently Asked Questions” page can be submitted to our company <strong>via a dedicated area for that purpose.</strong></p>
+      <p>Users should be able to communicate with us in order to <strong>increase the natural degree of interactions</strong> between buyers and our company.</p>
+      </div>
+      <div class="modal-footer">
+        <div class="container">
+      <div class="modal-body">
+      <span><a href="{{route('faq')}}"> More Questions?  </a></span> </div></div>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Got it, thanks!</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 @endsection
