bb7dc614b919be30a6f4a33717f7eb92fa949be9
Gonçalo Bernardo <up201606058@fe.up.pt>
2019-06-07 14:01:33 +0100

    pagination gone

--- resources/views/pages/categories.blade.php
+++ resources/views/pages/categories.blade.php
@@ -150,31 +150,6 @@
                 </div>
             </div>
         </div>
-        <nav class="mt-4 d-flex justify-content-end" aria-label="Page navigation">
-            <ul class="pagination">
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">1</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">2</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#">3</a>
-                </li>
-                <li class="page-item">
-                    <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
     </div>
 </main>
 
